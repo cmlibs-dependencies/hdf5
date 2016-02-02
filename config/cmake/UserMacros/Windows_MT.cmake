e4d1a79688ffbe0842293ce62aa9fb30c3a351ff
Daniel Wirtz <daniel.wirtz.stgt@gmail.com>
2016-02-02 17:14:46 +1300

    uncommented the /MD to /MT conversion for HDF5 - clashed with MS RT libs of all other dependencies

--- config/cmake/UserMacros/Windows_MT.cmake
+++ config/cmake/UserMacros/Windows_MT.cmake
@@ -17,9 +17,9 @@ MACRO (TARGET_STATIC_CRT_FLAGS)
         CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO
         CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
         CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-      if (${flag_var} MATCHES "/MD")
-        string (REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
-      endif (${flag_var} MATCHES "/MD")
+      #if (${flag_var} MATCHES "/MD")
+      #  string (REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      #endif (${flag_var} MATCHES "/MD")
     endforeach (flag_var)
     foreach (flag_var
         CMAKE_Fortran_FLAGS CMAKE_Fortran_FLAGS_DEBUG CMAKE_Fortran_FLAGS_RELEASE
