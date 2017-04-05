3716bf94659be0a4c9589e5f58112b4836867acc
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-04-06 11:16:35 +1200

    Add hdf5 config dependencies file (and install rule) for gathering external dependencies.

--- CMakeInstallation.cmake
+++ CMakeInstallation.cmake
@@ -126,6 +126,28 @@ if (NOT HDF5_EXTERNALLY_CONFIGURED)
 endif (NOT HDF5_EXTERNALLY_CONFIGURED)
 
 #-----------------------------------------------------------------------------
+# Configure the hdf5-config-version .cmake file for the install directory
+#-----------------------------------------------------------------------------
+if (NOT HDF5_EXTERNALLY_CONFIGURED)
+    file(WRITE ${HDF5_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${HDF5_PACKAGE}${HDF_PACKAGE_EXT}-config-dependencies.cmake 
+        "include(CMakeFindDependencyMacro)\r\n"
+	    "set(${PROJECT_NAME}_IMPORT_PREFIX \${_IMPORT_PREFIX})\r\n"
+    )
+	if(ZLIB_FOUND AND ZLIB_USE_EXTERNAL)
+		file(APPEND ${HDF5_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${HDF5_PACKAGE}${HDF_PACKAGE_EXT}-config-dependencies.cmake "find_dependency(ZLIB)\r\n")
+	endif()
+	if(SZIP_FOUND AND SZIP_USE_EXTERNAL)
+		file(APPEND ${HDF5_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${HDF5_PACKAGE}${HDF_PACKAGE_EXT}-config-dependencies.cmake "find_dependency(SZIP)\r\n")
+	endif()
+	file(APPEND ${HDF5_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${HDF5_PACKAGE}${HDF_PACKAGE_EXT}-config-dependencies.cmake "set(_IMPORT_PREFIX \${${PROJECT_NAME}_IMPORT_PREFIX})")    
+    install (
+        FILES ${HDF5_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${HDF5_PACKAGE}${HDF_PACKAGE_EXT}-config-dependencies.cmake
+        DESTINATION ${HDF5_INSTALL_CMAKE_DIR}
+        COMPONENT configinstall
+    )
+endif (NOT HDF5_EXTERNALLY_CONFIGURED)
+
+#-----------------------------------------------------------------------------
 # Configure the libhdf5.settings file for the lib info
 #-----------------------------------------------------------------------------
 if (H5_WORDS_BIGENDIAN)
